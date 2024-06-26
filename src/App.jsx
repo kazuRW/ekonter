dcc49b1cb38dde9ee8ad52770ef61a880607d08a
ramzdanigan <ramzdanigan@gmail.com>
2024-06-26 23:25:36 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,13 @@ function App() {
       <h3>Project</h3>
       <h5>Sistem Informasi Konter</h5>
       <div className="card">
+        <p>
+          Aplikasi Sistem Informasi Kebutuhan Konter
+        </p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
