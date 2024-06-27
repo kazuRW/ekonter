11a68b9937b89e319f8565a67fd4440f0efdc6f1
ramzdanigan <ramzdanigan@gmail.com>
2024-06-27 10:00:47 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ButtonDaisy from "./Components/ButtonDaisy";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -20,16 +21,15 @@ function App() {
       <h4>Rama + Bram</h4>
       <h3>Project</h3>
       <h5>Sistem Informasi Konter</h5>
+      <ButtonDaisy />
       <div className="card">
-        <p>
-          Aplikasi Sistem Informasi Kebutuhan Konter
-        </p>
+        <p>Aplikasi Sistem Informasi Kebutuhan Konter</p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
     </>
-  )
+  );
 }
 
 export default App
