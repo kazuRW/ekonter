03b7e0101f942913f1f302b9891b6edee64b1254
batavia0 <bramasto56964@gmail.com>
2024-07-11 11:10:13 +0700

    Fix Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Main from './Components/main'
 
 const App = () => {
   return (
-    <div className='bg-indigo-800'>
+    <div className='bg-blue-500 text-white p-4'>
       <Main/>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import './index.css'
 //import BrowserRouter dari react router
 import { BrowserRouter } from 'react-router-dom';
 
